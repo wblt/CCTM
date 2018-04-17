3d312d9ff72fd9cba636ca19eee7eff37ed79aa5
wblt <940422068@qq.com>
2018-04-17 16:42:51 +0800

    d

--- app/src/main/java/wb/com/cctm/adapter/CheckAdpter.java
+++ app/src/main/java/wb/com/cctm/adapter/CheckAdpter.java
@@ -5,10 +5,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import java.util.List;
 
 import wb.com.cctm.R;
+import wb.com.cctm.base.OnItemClickListener;
 
 /**
  * Created by wb on 2018/4/14.
@@ -18,6 +20,7 @@ public class CheckAdpter extends RecyclerView.Adapter{
 
     private List<String> datas;
     private Context context;
+    private OnItemClickListener<String> listener;
     public CheckAdpter(List<String> datas, Context context) {
         this.datas = datas;
         this.context = context;
@@ -31,8 +34,17 @@ public class CheckAdpter extends RecyclerView.Adapter{
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        Myholder myholder = (Myholder) holder;
+        final String s = datas.get(position);
+        myholder.ll_pipei.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (listener != null) {
+                    listener.onClick(s,view,position);
+                }
+            }
+        });
     }
 
     @Override
@@ -41,8 +53,15 @@ public class CheckAdpter extends RecyclerView.Adapter{
     }
 
     public static class Myholder extends RecyclerView.ViewHolder {
+        private LinearLayout ll_pipei;
         public Myholder(View itemView) {
             super(itemView);
+            ll_pipei = itemView.findViewById(R.id.ll_pipei);
         }
+    }
+
+
+    public void setListener(OnItemClickListener<String> listener) {
+        this.listener = listener;
     }
 }
--- app/src/main/java/wb/com/cctm/fragment/CheckFragment.java
+++ app/src/main/java/wb/com/cctm/fragment/CheckFragment.java
@@ -10,6 +10,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.lmj.mypwdinputlibrary.InputPwdView;
+import com.lmj.mypwdinputlibrary.MyInputPwdUtil;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,12 +22,15 @@ import butterknife.Unbinder;
 import wb.com.cctm.R;
 import wb.com.cctm.adapter.CheckAdpter;
 import wb.com.cctm.base.BaseFragment;
+import wb.com.cctm.base.OnItemClickListener;
+import wb.com.cctm.commons.utils.ToastUtils;
 
 
 public class CheckFragment extends BaseFragment {
     Unbinder unbinder;
     @BindView(R.id.recyc_list)
     RecyclerView recyc_list;
+    private MyInputPwdUtil myInputPwdUtil;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,8 +55,32 @@ public class CheckFragment extends BaseFragment {
         dates.add("");
         dates.add("");
         CheckAdpter adpter = new CheckAdpter(dates,getContext());
+        adpter.setListener(new OnItemClickListener<String>() {
+            @Override
+            public void onClick(String s, View view, int position) {
+                myInputPwdUtil.show();
+            }
+        });
         recyc_list.setLayoutManager(new LinearLayoutManager(getContext()));
         recyc_list.setAdapter(adpter);
+        myInputPwdUtil = new MyInputPwdUtil(getActivity());
+        myInputPwdUtil.getMyInputDialogBuilder().setAnimStyle(R.style.dialog_anim);
+        myInputPwdUtil.setListener(new InputPwdView.InputPwdListener() {
+            @Override
+            public void hide() {
+                myInputPwdUtil.hide();
+            }
+
+            @Override
+            public void forgetPwd() {
+                ToastUtils.toastutils("忘记密码",getContext());
+            }
+
+            @Override
+            public void finishPwd(String pwd) {
+                ToastUtils.toastutils(pwd,getContext());
+            }
+        });
     }
 
     @Override
