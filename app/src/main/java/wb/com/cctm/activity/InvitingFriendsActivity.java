defff4b1e27acde7884a7483bed1d44f157cfd64
wblt <940422068@qq.com>
2018-05-07 00:26:42 +0800

    d

--- app/src/main/java/wb/com/cctm/activity/InvitingFriendsActivity.java
+++ app/src/main/java/wb/com/cctm/activity/InvitingFriendsActivity.java
@@ -2,6 +2,7 @@ package wb.com.cctm.activity;
 
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,10 +27,12 @@ import wb.com.cctm.net.MXUtils;
 
 public class InvitingFriendsActivity extends BaseActivity {
 
-    @BindView(R.id.tv_left_copy)
-    TextView tv_left_copy;
-    @BindView(R.id.tv_right_copy)
-    TextView tv_right_copy;
+    @BindView(R.id.tv_left_addr)
+    TextView tv_left_addr;
+    @BindView(R.id.tv_right_addr)
+    TextView tv_right_addr;
+    @BindView(R.id.tv_app_addr)
+    TextView tv_app_addr;
     @BindView(R.id.iv_img_qrcode)
     ImageView iv_img_qrcode;
     private String left_str = "";
@@ -44,19 +47,36 @@ public class InvitingFriendsActivity extends BaseActivity {
         setTopBarTitle("邀请好友");
         ButterKnife.bind(this);
         invitation();
+        initveiw();
     }
 
-    @OnClick({R.id.tv_left_copy,R.id.tv_right_copy})
+    private void initveiw() {
+        tv_left_addr.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG); //下划线
+        tv_left_addr.getPaint().setAntiAlias(true);//抗锯齿
+
+        tv_right_addr.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG); //下划线
+        tv_right_addr.getPaint().setAntiAlias(true);//抗锯齿
+
+        tv_app_addr.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG); //下划线
+        tv_app_addr.getPaint().setAntiAlias(true);//抗锯齿
+    }
+
+    @OnClick({R.id.tv_left_addr,R.id.tv_right_addr,R.id.tv_app_addr})
     void viewClick(View view) {
         switch (view.getId()) {
-            case R.id.tv_left_copy:
+            case R.id.tv_left_addr:
                 ClipboardManager clip_left = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
                 clip_left.setText(left_str);
                 ToastUtils.toastutils("你已复制到粘贴板",InvitingFriendsActivity.this);
                 break;
-            case R.id.tv_right_copy:
+            case R.id.tv_right_addr:
                 ClipboardManager clip_right = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
                 clip_right.setText(right_str);
+                ToastUtils.toastutils("你已复制到粘贴板",InvitingFriendsActivity.this);
+                break;
+            case R.id.tv_app_addr:
+                ClipboardManager clip_app = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+                clip_app.setText(app_url);
                 ToastUtils.toastutils("你已复制到粘贴板",InvitingFriendsActivity.this);
                 break;
             default:
@@ -82,6 +102,9 @@ public class InvitingFriendsActivity extends BaseActivity {
                     right_str = pd_obj.getString("RIGHT_URL");
                     app_url = pd_obj.getString("APP_URL");
                     ImageLoader.load(app_url,iv_img_qrcode);
+                    tv_left_addr.setText(left_str);
+                    tv_right_addr.setText(right_str);
+                    tv_app_addr.setText(app_url);
                 } else {
                     ToastUtils.toastutils(message,InvitingFriendsActivity.this);
                 }
--- app/src/main/java/wb/com/cctm/fragment/DeliverFragment.java
+++ app/src/main/java/wb/com/cctm/fragment/DeliverFragment.java
@@ -168,8 +168,6 @@ public class DeliverFragment extends BaseFragment {
                 startActivity(intent);
                 break;
             case R.id.ll_suan_li:
-//                intent = new Intent(getActivity(), ShengHeActivity.class);
-//                startActivity(intent);
                 break;
             default:
                 break;
